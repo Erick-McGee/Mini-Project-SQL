7e6f27207ecfcab52aa5958b703d152235eae3bf
linotmike <128604219+linotmike@users.noreply.github.com>
2023-04-28 13:21:42 -0700

    added sql

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO movies(movies_name)
+VALUES ("Harry potter"),
+       ("ski patrol"),
+       ("Mt.rainier"),
+       ("Mt.kilimangaro")
+       ("The snow house");
+
+
+INSERT INTO reviews (reviews, movies)
+VALUES ("Movie about magic")
